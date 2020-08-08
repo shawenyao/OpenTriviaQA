640e6c495982f7702bd8b6bec739669a72db460e
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-08-08 13:44:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
+from os import listdir
 import numpy as np
 import random
 
-print("====================================")
-print("Welcome to OpenTriviaQA")
-print("====================================")
+print('====================================')
+print('Welcome to OpenTriviaQA')
+print('====================================')
+input('All available categories:')
 
-categories = [
-    "animals",
-    "brain-teasers",
-    "celebrities",
-    "entertainment",
-    "for-kids",
-    "general",
-    "geography",
-    "history",
-    "hobbies",
-    "humanities",
-    "literature",
-    "movies",
-    "music",
-    "newest",
-    "people",
-    "rated",
-    "religion-faith",
-    "science-technology",
-    "sports",
-    "television",
-    "video-games",
-    "world"]
+# show all available categories
+categories_all = listdir('categories/')
+for counter, value in enumerate(categories_all):
+    print(str(counter + 1) + ': ' + value)
 
-file_names = ["categories/" + category for category in categories]
+# let user specify categories of choice
+print('====================================')
+print('Input the categories of choice, separated by comma:')
+categories_input = input('')
 
-# a list of all the questions by category
+# parse user input
+categories_input_index = [int(index) - 1 for index in categories_input.split(",")]
+categories_input_index = set(np.clip(categories_input_index, 0, len(categories_all) - 1).tolist()) # avoid illegal index
+categories = [categories_all[i] for i in categories_input_index]
+
+# print selected categories
+print("Selected categories:")
+for counter, value in enumerate(categories):
+    print(str(counter + 1) + ': ' + value)
+print('====================================')
+
+# read all questions in selected categories
+file_names = ['categories/' + category for category in categories]
 lines_list = []
 for file_name in file_names:
     with open(file_name, encoding='utf-8', errors='replace') as f:
@@ -39,34 +37,36 @@ for file_name in file_names:
         lines = [line.rstrip('\n') for line in lines_raw]
     lines_list.append(lines)
 
+# game begins
 while True:
+    # loop through categories
     for counter, value in enumerate(categories):
 
-        print("Category: " + value)
+        print('Category: ' + value)
         input('')
 
         # find all rows where a question starts
-        index_start = np.where([line.startswith("#") for line in lines_list[counter]])[0]
+        index_start = np.where([line.startswith('#') for line in lines_list[counter]])[0]
 
         # randomly select the next question
         index = random.choice(index_start)
 
         while True:
-            # if it's the end of the question
-            if lines_list[counter][index] == "":
+            # if it's the end of the question, wait for user input to show the answer and break the loop
+            if lines_list[counter][index] == '':
                 input('')
 
                 # print the answer
                 print(lines_list[counter][index_answer])
-                print("====================================")
+                print('====================================')
                 break
 
-            # if it's the answer
-            elif lines_list[counter][index].startswith("^"):
+            # if it's the answer, remember the position
+            elif lines_list[counter][index].startswith('^'):
                 # remember where the answer is
                 index_answer = index
 
-            # if it's the question
+            # if it's the question, print it
             else:
                 print(lines_list[counter][index])
             index = index + 1
