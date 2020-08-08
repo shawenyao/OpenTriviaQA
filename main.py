5bbdf3fbb35363b0728acda44777edd6893cba89
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-08-07 23:02:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ categories = [
     "world"]
 file_names = ["categories/" + category for category in categories]
 
+# a list of all the questions by category
 lines_list = []
 for file_name in file_names:
     with open(file_name, encoding='utf-8', errors='replace') as f:
@@ -43,18 +44,28 @@ while True:
         print("Category: " + value)
         input('')
 
+        # find all rows where a question starts
         index_start = np.where([line.startswith("#") for line in lines_list[counter]])[0]
 
+        # randomly select the next question
         index = random.choice(index_start)
 
         while True:
+            # if it's the end of the question
             if lines_list[counter][index] == "":
                 input('')
+
+                # print the answer
                 print(lines_list[counter][index_answer])
                 print("====================================")
                 break
+
+            # if it's the answer
             elif lines_list[counter][index].startswith("^"):
+                # remember where the answer is
                 index_answer = index
+
+            # if it's the question
             else:
                 print(lines_list[counter][index])
             index = index + 1
